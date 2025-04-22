566d68d6f70f31d283dd674ebeb0c35c0a394ffd
“lujain“ <###lujain.alj.real@gmail.com>
2025-04-22 10:25:35 +0300

    ..

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,6 @@
 const orderController = require('../controllers/orderController');
 const { protect, restrictTo } = require('./../middlewares/authMiddlewers');
-const { addVarBody,addQuery } = require('./../middlewares/dynamicMiddleware');
+const { addVarBody, addQuery } = require('./../middlewares/dynamicMiddleware');
 const { RoleCode } = require('./../utils/enum');
 const { USER, ADMIN, DELIVERY } = RoleCode;
 const express = require('express');
@@ -8,11 +8,19 @@ const router = express.Router();
 router.use(protect);
 router
   .route('/mine')
-  .get(restrictTo( USER),addQuery("userId","userId"), orderController.getAllOrder)
+  .get(
+    restrictTo(USER),
+    addQuery('userId', 'userId'),
+    orderController.getAllOrder,
+  );
 router
   .route('/')
-  .get(restrictTo( ADMIN), orderController.getAllOrder)
-  .post(restrictTo(USER, ADMIN),addVarBody("userId","userId"), orderController.createOrder);
+  .get(restrictTo(ADMIN), orderController.getAllOrder)
+  .post(
+    restrictTo(USER, ADMIN),
+    addVarBody('userId', 'userId'),
+    orderController.createOrder,
+  );
 router
   .route('/:id')
   .get(restrictTo(USER, ADMIN, DELIVERY), orderController.getOrder)
